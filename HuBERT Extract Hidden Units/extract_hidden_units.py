3b3c759bf55af23357c2e0de92bef84397852e25
GhDamghani <130757093+GhDamghani@users.noreply.github.com>
2023-11-16 13:17:01 +0100

    Hidden units from each word
    With the assumption that words are not repetitive and '' leads to opting out the sound.

--- HuBERT Extract Hidden Units/extract_hidden_units.py
+++ HuBERT Extract Hidden Units/extract_hidden_units.py
@@ -3,17 +3,35 @@ from os.path import join, exists
 from os import mkdir
 import numpy as np
 from scipy.io.wavfile import read
+from tqdm import tqdm
 
 processor = AutoProcessor.from_pretrained("facebook/hubert-large-ls960-ft")
 model = HubertModel.from_pretrained("facebook/hubert-large-ls960-ft")
 
 result_dir = 'results'
+features_dir = join('..', 'SingleWordProductionDutch-main', 'features')
 if not exists(result_dir):
     mkdir(result_dir)
 
-for p_ind in range(1, 11):
-    sr, sound = read(join('..', 'SingleWordProductionDutch-main', 'features', f'sub-{p_ind:02}_orig_audio.wav'))
+eeg_sr = 1024
+
+for p_ind in tqdm(range(1, 11)):
+    audio_sr, sound = read(join(features_dir, f'sub-{p_ind:02}_orig_audio.wav'))
     sound = sound/32767
-    input_values = processor(sound, return_tensors="pt", sampling_rate=sr).input_values  # Batch size 1
-    hidden_states = model(input_values[:, :input_values.shape[1]//1000]).last_hidden_state.detach().numpy()
-    np.save(join(result_dir, f'sub-{p_ind:02}_hidden_units'), hidden_states)
+    words_arr = np.load(join(features_dir, f'sub-{p_ind:02}_procWords.npy'))
+    words = np.unique(words_arr)
+    words = words[np.where(words != b'')]
+    sub_dir = f'sub-{p_ind:02}'
+    if not exists(join(result_dir, sub_dir)):
+        mkdir(join(result_dir, sub_dir))
+    for word_i, word in tqdm(enumerate(words)):
+        word_ind = np.where(words_arr == word)[0][[0, -1]]
+        word_ind_start = np.int32(np.round_(word_ind[0]/eeg_sr * audio_sr))
+        word_ind_stop = np.int32(np.round_((word_ind[1]+1)/eeg_sr * audio_sr))
+        word_audio = sound[word_ind_start:word_ind_stop]
+        input_values = processor(word_audio, return_tensors="pt", sampling_rate=audio_sr).input_values  # Batch size 1
+        hidden_states = model(input_values).last_hidden_state.detach().numpy()
+        np.save(join(result_dir, sub_dir, f'{word_i:03}_{word.decode("ascii")}'), hidden_states)
+
+
+

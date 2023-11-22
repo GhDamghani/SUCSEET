99a736c559772f2685486b64f8d55c2985338320
GhDamghani <130757093+GhDamghani@users.noreply.github.com>
2023-11-22 16:44:23 +0100

    Correcting clustering approach

--- Kmeans of sounds/cluster.py
+++ Kmeans of sounds/cluster.py
@@ -9,10 +9,10 @@ participant = 'sub-06'
 
 melSpec = np.load(join(path_input, f'{participant}_spec.npy'))
 
-trainData = slider(melSpec)
+# trainData = slider(melSpec)
 
 kmeans = KMeans(n_clusters=20, random_state=0, n_init="auto")
-dists = kmeans.fit_transform(trainData)
+dists = kmeans.fit_transform(melSpec)
 lbl = kmeans.labels_
 keep=[]
 for i in range(dists.shape[1]):
